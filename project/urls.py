fe40a5e30e7f9330f1a9060379c81f0ee5be8313
YukiYucha <100174001+YukiYucha@users.noreply.github.com>
2022-06-28 20:37:11 +0900

    allauthによる認証#12

--- project/urls.py
+++ project/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('pakutter/',include('pakutter.urls')),
     path('admin/', admin.site.urls),
+    # auth
+    path('account/', include('allauth.urls')),
 ]
